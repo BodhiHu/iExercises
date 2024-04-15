b0531746395936ea727f7fcb2c73a8f7240d1483
BodhiCitta <3882561+BodhiHu@users.noreply.github.com>
2024-04-15 15:46:08 +0800

    update

--- playground/exam.py
+++ playground/exam.py
@@ -77,6 +77,85 @@ def search_word():
   print(matched_words[k])
 
 
+def encrypt():
+  line = input()
+  letters = list(line)
+  encrypted = []
+  for l in letters:
+    if l.isalpha():
+      if l == 'z':
+        l = 'a'
+      elif l == 'Z':
+        l = 'A'
+      else:
+        l = chr(ord(l) + 1)
+
+      l = l.lower() if l.isupper() else l.upper()
+    elif l.isnumeric():
+      l = str((int(l) + 1) % 10) 
+    
+    encrypted.append(l)
+
+  print("".join(encrypted))
+
+
+# input: 1 2 3 4 3 2 1 1 7
+# the last num is k, the rest are the array of numbers
+# find consecutive sub arrays that sum to k
+def count_subarrays():
+  line = input()
+  args = line.split(" ")
+  nums = [ int(num) for num in args[0:-1] ]
+  k = int(args[-1])
+
+  count = 0
+
+  for i in range(0, len(nums)):
+    sum = 0
+    for j in range(i, len(nums)):
+      if j == i:
+        sum = nums[i]
+      else:
+        sum += nums[j]
+      if sum >= k:
+        if sum == k:
+          count += 1
+        break
+    print("  debug >> ", i, sum)
+
+  print(count)
+
+
+# 3_ABAKKABA_7
+# AHHHA
+def longest_symmetric_substr():
+  import math
+  s = input()
+  longest_subs = ""
+  for i in range(0, len(s)-1):
+    for j in range(i+2, len(s)):
+      subs = s[i:j]
+      if len(subs) > math.ceil(len(s[i:])/2):
+        break
+
+      reversed_subs = "".join(reversed(subs))
+      if s[j-1:j-1+len(subs)] == reversed_subs:
+        concated = subs[i:j-1] + reversed_subs
+        if len(concated) > len(longest_subs):
+          longest_subs = concated
+      if s[j:j+len(subs)] == reversed_subs:
+        concated = subs + reversed_subs
+        if len(concated) > len(longest_subs):
+          longest_subs = concated
+
+      # print(subs, s[j-1:j-1+len(subs)], s[j:j+len(subs)])
+  
+  print(longest_subs)
+
+
 if __name__ == '__main__':
   # make_chore()
-  search_word()
+  # search_word()
+  # encrypt()
+  # count_subarrays()
+  longest_symmetric_substr()
